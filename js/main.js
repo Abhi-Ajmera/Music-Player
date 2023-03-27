78e6a690c956839aab363c7640666b433c757522
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-03-27 14:15:35 +0530

    js first commit

--- js/main.js
+++ js/main.js
@@ -1,104 +1,26 @@
-// const music = new Audio('Songs/295.mp3');
-
-// // Array
-// const Songs = [
-// {
-//     id:'1',
-//     songName:`Excuses <br>
-//     <div class="subtitle">AP Dhillon</div>`,
-//     poster: "../Img/Excuses.jpg"
-// },
-// {
-//     id:'2',
-//     songName:`Summer High <br>
-//     <div class="subtitle">AP Dhillon</div>`,
-//     poster: "../Img/Summer-High.jpg"
-// },
-// {
-//     id:'3',
-//     songName:`Sadda Pyar <br>
-//     <div class="subtitle">AP Dhillon</div>`,
-//     poster: "../Img/AP-Dhillon.jpg"
-// },
-// {
-//     id:'4',
-//     songName:`INDUSTRY-BABY <br>
-//     <div class="subtitle">Lil Nasx</div>`,
-//     poster: "../Img/INDUSTRY-BABY.jpg"
-// },
-// {
-//     id:'5',
-//     songName:`MONTERO <br>
-//     <div class="subtitle">Lil Nasx</div>`,
-//     poster: "../Img/INDUSTRY-BABY.jpg"
-// },
-// {
-//     id:'6',
-//     songName:`295 <br>
-//     <div class="subtitle">Sidhu Moosewala</div>`,
-//     poster: "../Img/Moosetape.jpg"
-// },
-// {
-//     id:'7',
-//     songName:`Old_Skool <br>
-//     <div class="subtitle">Sidhu Moosewala</div>`,
-//     poster: "../Img/sidhu-moosewala.jpg"
-// },
-// {
-//     id:'8',
-//     songName:`Fuck_Em_All <br>
-//     <div class="subtitle">Sidhu Moosewala</div>`,
-//     poster: "../Img/Moosetape.jpg"
-// },
-// {
-//     id:'9',
-//     songName:`Money <br>
-//     <div class="subtitle">Blackpink</div>`,
-//     poster: "../Img/LALISA.jpg"
-// },
-// {
-//     id:'10',
-//     songName:`How_You_Like_That <br>
-//     <div class="subtitle">Blackpink</div>`,
-//     poster: "../Img/LALISA.jpg"
-// },
-// {
-//     id:'11',
-//     songName:`NO-LOVE <br>
-//     <div class="subtitle">Shubh</div>`,
-//     poster: "../Img/NO-LOVE.jpg"
-// },
-// {
-//     id:'12',
-//     songName:`Pasoori <br>
-//     <div class="subtitle">Shae Gill</div>`,
-//     poster: "../Img/Pasoori.jpg"
-// },
-// {
-//     id:'13',
-//     songName:`Starboy <br>
-//     <div class="subtitle">Weekend</div>`,
-//     poster: "../Img/Starboy.jpg"
-// },
-// {
-//     id:'14',
-//     songName:`Sugar-Brownies <br>
-//     <div class="subtitle">Sia</div>`,
-//     poster: "../Img/Sugar-Brownies.jpg"
-// },
-// {
-//     id:'15',
-//     songName:`Unstoppable <br>
-//     <div class="subtitle">Sia</div>`,
-//     poster: "../Img/Unstoppable.jpg"
-// }
-// ]
-
-// Array.from(document.getElementsByClassName("songItem")).forEach((element,i)=>{
-//     element.getElementsByTagName('img')[0].src = Songs[i].poster;
-//     element.getElementsByTagName('h5')[0].innerHTML = Songs[i].songName;
-// })
 
 
-// start from here
 
+let pop_song_left = document.getElementById("pop_song_left");
+let pop_song_right = document.getElementById("pop_song_right");
+let pop_song = document.getElementsByClassName("pop_song")[0];
+
+pop_song_right.addEventListener('click', () => {
+    pop_song.scrollLeft += 400;
+});
+
+pop_song_left.addEventListener('click', () => {
+    pop_song.scrollLeft -= 400;
+});
+
+let pop_artist_left = document.getElementById('pop_artist_left');
+let pop_artist_right = document.getElementById('pop_artist_right');
+let pop_artist = document.getElementsByClassName('item')[0];
+
+pop_artist_right.addEventListener('click', () => {
+    pop_artist.scrollLeft += 400;
+});
+
+pop_artist_left.addEventListener('click', () => {
+    pop_artist.scrollLeft -= 400;
+});
