87333b853beb2aff490fcd3ee084ba0b6d6918ee
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-04-17 23:36:19 +0530

    more js added

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const songs = [
         id:'1',
         songName: '295',
         artist: 'Sidhu Moosewala',
-        poster:'Img/Moosetrap.jpg'
+        poster:'Img/Moosetape.jpg'
     },
     {
         id:'2',
@@ -42,7 +42,7 @@ const songs = [
         id:'7',
         songName: 'Montero',
         artist: 'Lil NasX',
-        poster:'Img/Insustry-baby.jpg'
+        poster:'Img/Industry-baby.jpg'
     },
     {
         id:'8',
@@ -54,7 +54,7 @@ const songs = [
         id:'9',
         songName: 'Old Skool',
         artist: 'Sidhu Moosewala',
-        poster:'Img/Moosetrap.jpg'
+        poster:'Img/Moosetape.jpg'
     },
     {
         id:'10',
@@ -85,9 +85,60 @@ const songs = [
         songName: 'Unstoppable',
         artist: 'Sia',
         poster:'Img/Unstoppable.jpg'
+    },
+    {
+        id:'15',
+        songName: 'To be added',
+        artist: 'To be added',
+        poster:'Img/Unstoppable.jpg'
+    },
+    {
+        id:'16',
+        songName: 'To be added',
+        artist: 'To be added',
+        poster:'Img/Unstoppable.jpg'
+    },
+    {
+        id:'17',
+        songName: 'To be added',
+        artist: 'To be added',
+        poster:'Img/Unstoppable.jpg'
+    },
+    {
+        id:'18',
+        songName: 'To be added',
+        artist: 'To be added',
+        poster:'Img/Unstoppable.jpg'
     }
 ];
 
+// data for popular songs
+Array.from(document.getElementsByClassName("songItem")).forEach((element,index)=>{
+    element.getElementsByTagName("img")[0].src = songs[index].poster;
+    element.querySelectorAll("h5 span")[0].innerText = songs[index].songName;
+    element.querySelectorAll("h5 .subtitle")[0].innerText = songs[index].artist;
+});
+
+// data for popular artist
+Array.from(document.querySelectorAll(".item li")).forEach((element,index)=>{
+    element.getElementsByTagName("img")[0].src = songs[index].poster;
+});
+
+let masterPlay = document.querySelector("#masterPlay");
+let wave = document.querySelector("#wave");
+
+masterPlay.addEventListener("click",()=>{
+    if(music.paused || music.currentTime <=0){
+        music.play();
+        wave.classList.add("active2");
+    } else {
+        music.pause();
+        wave.classList.remove("active2");
+    }
+});
+
+
+// for songs queue
 let pop_song_left = document.getElementById("pop_song_left");
 let pop_song_right = document.getElementById("pop_song_right");
 let pop_song = document.getElementsByClassName("pop_song")[0];
@@ -100,6 +151,7 @@ pop_song_left.addEventListener('click', () => {
     pop_song.scrollLeft -= 400;
 });
 
+// for artist queue
 let pop_artist_left = document.getElementById('pop_artist_left');
 let pop_artist_right = document.getElementById('pop_artist_right');
 let pop_artist = document.getElementsByClassName('item')[0];
